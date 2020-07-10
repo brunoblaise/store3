f8a87a06769b60af8da19e76cc75e67b30318717
brunoblaise <brunoblaise94@gmail.com>
2020-07-10 09:07:54 +0200

    header
    ok header done

---
+++ src/componets/homeTab.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import './homeTab.css'
+export default class HomeTab extends Component {
+    render() {
+        return (
+            <div>
+                <header className="main-header">
+                    {this.props.products.map(( (product)=>(
+ <ul>
+     <img src={product.logo} alt={product._id}></img>
+ <li>
+     <a href={"#" + product._id}> home</a>
+     </li>
+     <li>
+     <a href={"#" + product._id}>about</a>
+     </li>
+     <li>
+     <a href={"#" + product._id}>service</a>
+     </li>
+   <a className="user">user</a>
+</ul>
+                     ) ))
+                  
+    }
+                </header>
+            </div>
+        )
+    }
+}
+
